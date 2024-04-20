e0815d0002d5659bcabe0724e2dc641ca98f1ae6
s-u-r-e-n-d-r-a <153289436+s-u-r-e-n-d-r-a@users.noreply.github.com>
2024-04-20 20:20:54 +0530

    travel

---
+++ frontend/src/services/ServiceList.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ServiceCard from './ServiceCard'
+import { Col } from 'reactstrap'
+import weatherImg from '../assets/images/weather.png'
+import guideImg from '../assets/images/guide.png'
+import customizationImg from '../assets/images/customization.png'
+
+const servicesData = [
+   {
+      imgUrl: weatherImg,
+      title: `Calculate Weather`,
+      desc: `Our pleasure to give comfortable journey.`,
+   },
+   {
+      imgUrl: guideImg,
+      title: `Best Tour Guide`,
+      desc: `Our pleasure to give comfortable journey.`,
+   },
+   {
+      imgUrl: customizationImg,
+      title: 'Customization',
+      desc: `Our pleasure to give comfortable journey.`,
+   },
+]
+
+const ServiceList = () => {
+   return <>
+      {
+         servicesData.map((item, index) => (
+            <Col lg='3' md='6' sm='12' className='mb-4' key={index}>
+               <ServiceCard item={item} />
+            </Col>))
+      }
+   </>
+
+}
+
+export default ServiceList
